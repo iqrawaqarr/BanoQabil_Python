1233f1b0c0d40581775a63d0878cc2afc3647ff2
iqrawaqarr <157203849+iqrawaqarr@users.noreply.github.com>
2024-01-21 12:36:16 +0500

    Add files via upload

---
+++ iqra_midterm.py
@@ -0,0 +1,42 @@
+#iqra
+
+#Q1 - add two variables,division,power,floor and print thier sum 
+num1 = 5
+num2 = 4
+sum=num1+num2
+print("the sum of",num1,"+",num2,"is",sum)
+
+num1 = 5
+num2 = 4
+division=num1 / num2 
+print("the division of",num1,"/",num2,"is",division)
+
+num1 = 5
+num2 = 4
+power=num1 ^ num2
+print("the power of",num1,"^",num2,"is",power)
+
+num1 = 5
+num2 = 4
+floor =num1 // num2
+print("the floor division ",num1,"//",num2,"is",floor)
+
+#Q 2_ write a python program to find the area of a triangle
+
+l=10
+h=15
+area=1/2*l*h
+print("the area of triangle",l,"*",h,"is",area)
+
+# Q4- write a python that return all data types 
+print(10)
+print("data type:",type(10))
+print(10.5)
+print("data type:",type(10.5))
+print(Hello woarld)
+print("data type:",type(Hello world ))
+
+print([1,2,3,4,5])
+print("data type:",type[1,2,3,4,5])
+print({1,2,3,4,5})
+print("data type:",type,{1,2,3,4,5})
